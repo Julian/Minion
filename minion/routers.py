29a53f0163e4ce6e0ff4c64ce464cb70a3bb9e0d
Julian Berman <Julian@GrayVines.com>
2013-09-17 19:55:56 -0400

    Add a RoutesRouter.

--- minion/routers.py
+++ minion/routers.py
@@ -1,3 +1,36 @@
+"""
+Fundamental routing building blocks: routers and dispatchers.
+
+This module defines implementations of the two central routing objects:
+routers and dispatchers.
+
+A router maps URLs to "view" names. A dispatcher calls a view and passes along
+any additional arguments (besides the request object).
+
+"""
+
+try:
+    import routes
+except ImportError:
+    routes = None
+
+
+if routes is not None:
+    class RoutesRouter(object):
+        def __init__(self, mapper=None):
+            if mapper is None:
+                mapper = routes.Mapper()
+            self._mapper = mapper
+
+        def add_route(self, route, fn, **kwargs):
+            self._mapper.connect(None, route, minion_target=fn, **kwargs)
+
+        def match(self, request):
+            match = self._mapper.match(request.path)
+            fn = match.pop("minion_target")
+            return fn, match
+
+
 class SimpleRouter(object):
     def __init__(self, routes=None):
         if routes is None:
--- minion/tests/test_routers.py
+++ minion/tests/test_routers.py
@@ -1,8 +1,26 @@
-from unittest import TestCase
+from unittest import TestCase, skipIf
 import mock
 
 from minion import routers
 from minion.request import Request
+
+
+@skipIf(routers.routes is None, "routes not found")
+class TestRoutesRouter(TestCase):
+    def setUp(self):
+        self.router = routers.RoutesRouter()
+
+    def test_it_routes_routes(self):
+        fn = mock.Mock()
+        self.router.add_route("/route", fn)
+        matched = self.router.match(Request(path="/route"))
+        self.assertEqual(matched, (fn, {}))
+
+    def test_it_routes_routes_with_arguments(self):
+        fn = mock.Mock()
+        self.router.add_route("/route/{year}", fn, stuff="12")
+        matched = self.router.match(Request(path="/route/2013"))
+        self.assertEqual(matched, (fn, {"year" : "2013", "stuff" : "12"}))
 
 
 class TestSimpleRouter(TestCase):
