fe675ff13d7351bb5188ba7a7475dea280229f03
Julian Berman <Julian@GrayVines.com>
2013-09-17 20:58:44 -0400

    Multiple needs.

--- minion/tests/test_resource.py
+++ minion/tests/test_resource.py
@@ -5,15 +5,31 @@ from minion import resource
 
 
 class TestResourceBin(TestCase):
-    def test_it_stores_resources(self):
-        bin = resource.Bin()
+    def setUp(self):
+        self.bin = resource.Bin()
 
-        @bin.provides("iron")
+    def test_it_stores_resources(self):
+        @self.bin.provides("iron")
         def make_iron():
             return 12
 
         fn = mock.Mock(__name__="a_view")
-        returned = bin.needs(["iron"])(fn)(1, "foo", bar=3)
+        returned = self.bin.needs(["iron"])(fn)(1, "foo", bar=3)
 
         self.assertIs(returned, fn.return_value)
         fn.assert_called_once_with(1, "foo", bar=3, iron=12)
+
+    def test_multiple_needs(self):
+        @self.bin.provides("iron")
+        def make_iron():
+            return 12
+
+        thing = mock.Mock()
+        @self.bin.provides("wine")
+        def make_wine():
+            return thing
+
+        fn = mock.Mock(__name__="a_view")
+        self.bin.needs(["wine", "iron"])(fn)(1)
+
+        fn.assert_called_once_with(1, iron=12, wine=thing)
