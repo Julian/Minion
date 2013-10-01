11fb8e70f47c5e15d334dca9f37dffcc3b56bb41
Julian Berman <Julian@GrayVines.com>
2013-09-30 20:04:08 -0400

    Add basic route generation.

--- minion/routers.py
+++ minion/routers.py
@@ -6,6 +6,9 @@ This module defines (different) implementations of routers -- objects which map
 
 """
 
+import urllib
+
+
 try:
     import routes
 except ImportError:
@@ -15,12 +18,13 @@ else:
         def __init__(self, mapper=None):
             if mapper is None:
                 mapper = routes.Mapper()
+            self._generator = routes.URLGenerator(mapper, {})  # XXX: environ
             self._mapper = mapper
 
-        def add_route(self, route, fn, methods=None, **kwargs):
+        def add_route(self, route, fn, route_name=None, methods=None, **kwargs):
             if methods is not None:
                 kwargs.setdefault("conditions", {})["method"] = methods
-            self._mapper.connect(None, route, minion_target=fn, **kwargs)
+            self._mapper.connect(route_name, route, minion_target=fn, **kwargs)
 
         def match(self, request):
             match = self._mapper.match(
@@ -34,6 +38,9 @@ else:
             fn = match.pop("minion_target")
             return fn, match
 
+        def url_for(self, route_name, **kwargs):
+            return self._generator(route_name, **kwargs)
+
 
 try:
     import werkzeug.routing
@@ -44,13 +51,16 @@ else:
         def __init__(self, map=None):
             if map is None:
                 map = werkzeug.routing.Map()
+            self._endpoints = {}
             self._map = map
             self._adapter = self._map.bind("")  # XXX: server_name
 
-        def add_route(self, route, fn, methods=None, **kwargs):
+        def add_route(self, route, fn, route_name=None, methods=None, **kwargs):
             if methods is not None:
                 kwargs["methods"] = methods
-            rule = werkzeug.routing.Rule(route, endpoint=fn, **kwargs)
+            endpoint = route_name or fn
+            self._endpoints[endpoint] = fn
+            rule = werkzeug.routing.Rule(route, endpoint=endpoint, **kwargs)
             self._map.add(rule)
 
         def match(self, request):
@@ -59,19 +69,37 @@ else:
                 return None, {}
             return self._adapter.match(path_info=request.path, method=method)
 
+        def url_for(self, route_name, **kwargs):
+            try:
+                return self._adapter.build(route_name, kwargs)
+            except werkzeug.routing.BuildError as error:
+                return route_name
+
 
 
 class SimpleRouter(object):
-    def __init__(self, routes=None):
+    def __init__(self, routes=None, names=None):
         if routes is None:
             routes = {}
+        if names is None:
+            names = {}
         self.routes = routes
+        self._names = names
 
-    def add_route(self, route, fn, methods=("GET", "HEAD")):
+    def add_route(self, route, fn, route_name=None, methods=("GET", "HEAD")):
         self.routes[route] = fn, methods
+        if route_name is not None:
+            self._names[route_name] = route
 
     def match(self, request):
         fn, methods = self.routes.get(request.path, (None, None))
         if methods is not None and request.method not in methods:
             return None, {}
         return fn, {}
+
+    def url_for(self, route_name, **kwargs):
+        url = self._names.get(route_name, route_name)
+        query = urllib.urlencode(kwargs)
+        if query:
+            url += "?" + query
+        return url
--- minion/tests/test_routers.py
+++ minion/tests/test_routers.py
@@ -32,9 +32,22 @@ class RouterTestMixin(object):
         matched = self.router.match(Request(path="/route", method="GET"))
         self.assertEqual(matched, (None, {}))
 
+    def test_it_can_build_named_routes(self):
+        self.router.add_route("/", mock.Mock(), route_name="home")
+        self.assertEqual(self.router.url_for("home"), "/")
+
+    def test_unknown_route_names_become_literal_paths(self):
+        self.assertEqual(self.router.url_for("/work"), "/work")
+
     def test_it_does_not_route_unknown_paths(self):
         matched = self.router.match(Request(path="/route"))
         self.assertEqual(matched, (None, {}))
+
+    def test_extra_build_arguments_become_query_strings(self):
+        fn = mock.Mock()
+        self.router.add_route("/", fn, route_name="home")
+        url = self.router.url_for("home", thing="yes")
+        self.assertEqual(url, "/?thing=yes")
 
 
 @skipIf(not hasattr(routers, "RoutesRouter"), "routes not found")
@@ -48,6 +61,12 @@ class TestRoutesRouter(RouterTestMixin, TestCase):
         matched = self.router.match(Request(path="/route/2013"))
         self.assertEqual(matched, (fn, {"year" : "2013", "stuff" : "12"}))
 
+    def test_it_builds_routes_with_arguments(self):
+        fn = mock.Mock()
+        self.router.add_route("/{year}", fn, route_name="year")
+        url = self.router.url_for("year", year=2012)
+        self.assertEqual(url, "/2012")
+
 
 @skipIf(not hasattr(routers, "WerkzeugRouter"), "werkzeug not found")
 class TestWerkzeugRouter(RouterTestMixin, TestCase):
@@ -59,6 +78,12 @@ class TestWerkzeugRouter(RouterTestMixin, TestCase):
         self.router.add_route("/route/<int:year>", fn)
         matched = self.router.match(Request(path="/route/2013"))
         self.assertEqual(matched, (fn, {"year" : 2013}))
+
+    def test_it_builds_routes_with_arguments(self):
+        fn = mock.Mock()
+        self.router.add_route("/<int:year>", fn, route_name="year")
+        url = self.router.url_for("year", year=2012)
+        self.assertEqual(url, "/2012")
 
 
 class TestSimpleRouter(RouterTestMixin, TestCase):
