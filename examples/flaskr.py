36266d6c9d3688dbe252f841788a846511f01abe
Julian Berman <Julian@GrayVines.com>
2013-10-01 22:52:55 -0400

    Update flaskr example.

--- examples/flaskr.py
+++ examples/flaskr.py
@@ -21,22 +21,27 @@ config = {
     "credentials" : {"user" : "admin", "password" : "default"},
     "database" : {"uri" : "/tmp/flaskr.db"},
 }
-app = Application(router=WerkzeugRouter())
-bin = resource.Bin()
+loader = jinja2.FileSystemLoader(
+    os.path.join(os.path.dirname(__file__), "templates", "flaskr"),
+)
+app = Application(
+    jinja=jinja2.Environment(loader=loader),
+    router=WerkzeugRouter(),
+)
 
 
 @app.route("/")
-@bin.needs(["db", "j2env"])
-def show_entries(request, db, j2env):
+@app.bin.needs(["db", "jinja"])
+def show_entries(request, db, jinja):
     cur = db.execute("SELECT title, text FROM entries ORDER BY id DESC")
     entries = cur.fetchall()
-    content = j2env.get_template("show_entries.html").render(entries=entries)
+    content = jinja.get_template("show_entries.html").render(entries=entries)
     return Response(content.encode("utf-8"))
 
 
 @app.route('/add', methods=['POST'])
-@bin.needs(["db", "j2env"])
-def add_entry(request, db, j2env):
+@app.bin.needs(["db", "jinja"])
+def add_entry(request, db, jinja):
     # if not session.get('logged_in'):
     #     return Response(code=401)
     db.execute(
@@ -50,8 +55,8 @@ def add_entry(request, db, j2env):
 
 
 @app.route("/login", methods=["GET", "POST"])
-@bin.needs(["j2env"])
-def login(request, j2env):
+@app.bin.needs(["jinja"])
+def login(request, jinja):
     error = None
     if request.method == "POST":
         if request.form["username"] != config["credentials"]["user"]:
@@ -63,7 +68,7 @@ def login(request, j2env):
             # flash("You were logged in")
             # return redirect(url_for("show_entries"))
             return Response(code=302, headers={"Location" : "/"})
-    content = j2env.get_template("login.html").render(error=error)
+    content = jinja.get_template("login.html").render(error=error)
     return Response(content.encode("utf-8"))
 
 
@@ -81,7 +86,7 @@ def style(request):
         return Response(css.read())
 
 
-@bin.provides("db")
+@app.bin.provides("db")
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(config["database"]["uri"])
@@ -106,79 +111,6 @@ def init_db():
 
 
 wsgi = wsgi_app(app, request_class=werkzeug.wrappers.Request)
-loader = jinja2.DictLoader(
-    dict(
-        (k, textwrap.dedent(v)) for k, v in (
-            (
-                "layout.html", """
-                    <!doctype html>
-                    <title>Flaskr</title>
-                    <link rel=stylesheet type=text/css href="/static/flaskr.css">
-                    <div class=page>
-                    <h1>Flaskr</h1>
-                    {#
-                    <div class=metanav>
-                    {% if not session.logged_in %}
-                        <a href="/login">log in</a>
-                    {% else %}
-                        <a href="/logout">log out</a>
-                    {% endif %}
-                    </div>
-                    {% for message in get_flashed_messages() %}
-                        <div class=flash>{{ message }}</div>
-                    {% endfor %}
-                    #}
-                    {% block body %}{% endblock %}
-                    </div>
-                """
-            ),
-            (
-                "show_entries.html", """
-                    {% extends "layout.html" %}
-                    {% block body %}
-                    {# {% if session.logged_in %} #}
-                        <form action="/add" method=post class=add-entry>
-                        <dl>
-                            <dt>Title:
-                            <dd><input type=text size=30 name=title>
-                            <dt>Text:
-                            <dd><textarea name=text rows=5 cols=40></textarea>
-                            <dd><input type=submit value=Share>
-                        </dl>
-                        </form>
-                    {# {% endif %} #}
-                    <ul class=entries>
-                    {% for entry in entries %}
-                        <li><h2>{{ entry.title }}</h2>{{ entry.text|safe }}
-                    {% else %}
-                        <li><em>Unbelievable.  No entries here so far</em>
-                    {% endfor %}
-                    </ul>
-                    {% endblock %}
-                """
-            ),
-            (
-                "login.html", """
-                    {% extends "layout.html" %}
-                    {% block body %}
-                    <h2>Login</h2>
-                    {% if error %}<p class=error><strong>Error:</strong> {{ error }}{% endif %}
-                    <form action="/login" method=post>
-                        <dl>
-                        <dt>Username:
-                        <dd><input type=text name=username>
-                        <dt>Password:
-                        <dd><input type=password name=password>
-                        <dd><input type=submit value=Login>
-                        </dl>
-                    </form>
-                    {% endblock %}
-                """
-            ),
-        )
-    )
-)
-bin.globals["j2env"] = jinja2.Environment(loader=loader)
 
 
 if __name__ == "__main__":
