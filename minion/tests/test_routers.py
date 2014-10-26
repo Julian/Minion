6d9aa4b02e73a9ddbfef0dcc7b1008574fec5d50
Julian Berman <Julian@GrayVines.com>
2014-10-26 08:57:50 -0400

    Use a real view.

--- minion/tests/test_routers.py
+++ minion/tests/test_routers.py
@@ -1,8 +1,11 @@
 from unittest import TestCase, skipIf
-import mock
 
-from minion import routers
+from minion import Response, routers
 from minion.request import Request
+
+
+def view(request):
+    return Response("Is anybody out there?")
 
 
 class RouterTestMixin(object):
@@ -15,25 +18,22 @@ class RouterTestMixin(object):
     """
 
     def test_it_routes_routes(self):
-        fn = mock.Mock()
-        self.router.add_route("/route", fn)
+        self.router.add_route("/route", view)
         matched = self.router.match(Request(path="/route"))
-        self.assertEqual(matched, (fn, {}))
+        self.assertEqual(matched, (view, {}))
 
     def test_it_routes_routes_for_specified_methods(self):
-        fn = mock.Mock()
-        self.router.add_route("/route", fn, methods=["POST"])
+        self.router.add_route("/route", view, methods=["POST"])
         matched = self.router.match(Request(path="/route", method="POST"))
-        self.assertEqual(matched, (fn, {}))
+        self.assertEqual(matched, (view, {}))
 
     def test_it_does_not_route_routes_for_unspecified_methods(self):
-        fn = mock.Mock()
-        self.router.add_route("/route", fn, methods=["POST"])
+        self.router.add_route("/route", view, methods=["POST"])
         matched = self.router.match(Request(path="/route", method="GET"))
         self.assertEqual(matched, (None, {}))
 
     def test_it_can_build_named_routes(self):
-        self.router.add_route("/", mock.Mock(), route_name="home")
+        self.router.add_route("/", view, route_name="home")
         self.assertEqual(self.router.url_for("home"), "/")
 
     def test_unknown_route_names_become_literal_paths(self):
@@ -44,8 +44,7 @@ class RouterTestMixin(object):
         self.assertEqual(matched, (None, {}))
 
     def test_extra_build_arguments_become_query_strings(self):
-        fn = mock.Mock()
-        self.router.add_route("/", fn, route_name="home")
+        self.router.add_route("/", view, route_name="home")
         url = self.router.url_for("home", thing="yes")
         self.assertEqual(url, "/?thing=yes")
 
@@ -56,14 +55,12 @@ class TestRoutesRouter(RouterTestMixin, TestCase):
         self.router = routers.RoutesRouter()
 
     def test_it_routes_routes_with_arguments(self):
-        fn = mock.Mock()
-        self.router.add_route("/route/{year}", fn, stuff="12")
+        self.router.add_route("/route/{year}", view, stuff="12")
         matched = self.router.match(Request(path="/route/2013"))
-        self.assertEqual(matched, (fn, {"year" : "2013", "stuff" : "12"}))
+        self.assertEqual(matched, (view, {"year" : "2013", "stuff" : "12"}))
 
     def test_it_builds_routes_with_arguments(self):
-        fn = mock.Mock()
-        self.router.add_route("/{year}", fn, route_name="year")
+        self.router.add_route("/{year}", view, route_name="year")
         url = self.router.url_for("year", year=2012)
         self.assertEqual(url, "/2012")
 
@@ -74,14 +71,12 @@ class TestWerkzeugRouter(RouterTestMixin, TestCase):
         self.router = routers.WerkzeugRouter()
 
     def test_it_routes_routes_with_arguments(self):
-        fn = mock.Mock()
-        self.router.add_route("/route/<int:year>", fn)
+        self.router.add_route("/route/<int:year>", view)
         matched = self.router.match(Request(path="/route/2013"))
-        self.assertEqual(matched, (fn, {"year" : 2013}))
+        self.assertEqual(matched, (view, {"year" : 2013}))
 
     def test_it_builds_routes_with_arguments(self):
-        fn = mock.Mock()
-        self.router.add_route("/<int:year>", fn, route_name="year")
+        self.router.add_route("/<int:year>", view, route_name="year")
         url = self.router.url_for("year", year=2012)
         self.assertEqual(url, "/2012")
 
