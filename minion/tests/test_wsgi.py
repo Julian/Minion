0f001e0f39ad63df47b705b8e825d3f865071e17
Julian Berman <Julian@GrayVines.com>
2014-11-17 20:27:43 -0500

    DRY

--- minion/tests/test_wsgi.py
+++ minion/tests/test_wsgi.py
@@ -9,41 +9,38 @@ from minion.wsgi import wsgi_app
 
 
 class TestWSGIMinion(TestCase):
-    def test_it_speaks_wsgi(self):
-        minion = Application()
+    def setUp(self):
+        self.minion = Application()
+        self.wsgi = TestApp(wsgi_app(self.minion))
 
-        @minion.route("/respond")
-        def show(request):
+    def test_it_speaks_wsgi(self):
+        @self.minion.route(b"/respond")
+        def respond(request):
             return Response(b"Yep!")
 
-        wsgi = TestApp(wsgi_app(minion))
-        response = wsgi.get("/respond", status=200)
+        response = self.wsgi.get("/respond", status=200)
         self.assertEqual(response.body, b"Yep!")
 
     def test_it_parses_headers(self):
-        minion = Application()
-
-        @minion.route("/respond")
-        def show(request):
+        @self.minion.route(b"/respond")
+        def respond(request):
             return Response(request.headers.get("Accept")[0])
 
-        wsgi = TestApp(wsgi_app(minion))
-        response = wsgi.get("/respond", status=200, headers={b"Accept" : b"2"})
+        response = self.wsgi.get(
+            "/respond", status=200, headers={b"Accept" : b"2"},
+        )
         self.assertEqual(response.body, b"2")
 
     @skipIf(PY3, "WSGI is pure insanity on Py3")
     def test_it_sets_headers(self):
-        minion = Application()
-
-        @minion.route(b"/respond")
-        def show(request):
+        @self.minion.route(b"/respond")
+        def respond(request):
             return Response(
                 b"{}",
                 headers=Headers([(b"Content-Type", [b"application/json"])]),
             )
 
-        wsgi = TestApp(wsgi_app(minion))
-        response = wsgi.get(b"/respond", status=200)
+        response = self.wsgi.get(b"/respond", status=200)
         self.assertEqual(
             response.headers[b"Content-Type"],
             b"application/json",
